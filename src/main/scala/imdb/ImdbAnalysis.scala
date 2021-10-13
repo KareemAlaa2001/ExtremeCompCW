e9c2f21ec38e83a41af2575b7ac6185c0a136d4d
KareemAlaa2001 <kareemalaa2001@gmail.com>
2021-10-13 12:26:58 +0100

    cleaned up task 2 implementation and added an iomplementation for task 3

--- src/main/scala/imdb/ImdbAnalysis.scala
+++ src/main/scala/imdb/ImdbAnalysis.scala
@@ -1,5 +1,6 @@
 package imdb
 
+import scala.collection.immutable.ListMap
 import scala.io.Source
 
 
@@ -34,16 +35,6 @@ object ImdbAnalysis {
   // Hint: use a combination of `ImdbData.nameBasicsPath` and `ImdbData.parseNameBasics`
   val nameBasicsList: List[NameBasics] = extractStringListFromPath(ImdbData.nameBasicsPath, ImdbData.parseNameBasics)
 
-  def constructSingleGenreTitleBasicsObject(oldTitleBasics: TitleBasics, genre: String): TitleBasics = {
-    oldTitleBasics.copy(genres=Some(List(genre)))
-  }
-
-  def constructTitleBasicsListForAllGenresFromSingleObject(titleBasics: TitleBasics): List[TitleBasics] = {
-    assert(titleBasics.genres.isDefined)
-    val genresList: List[String] = titleBasics.genres.get
-    genresList.foldRight(List[TitleBasics]())((genre, acc) => titleBasics.copy(genres=Some(List(genre))) :: acc)
-  }
-
   def average(s: List[Int]): Float = {
     s.foldLeft((0f, 1f)) { case ((avg, id), curr) => (avg + (curr - avg)/id, id + 1) }._1
   }
@@ -66,8 +57,8 @@ object ImdbAnalysis {
     //  then flatmap such that each titlebasic object is mapped to a list of titlebasic objects, each with a different genre. could do a fold?
     //  then groupby genre - NOTE: need to account for none genres, single genres and multiple genres.
     //  then for each group, run by
-    val noneGenresAndRuntimesRemoved = list.filter(_.genres.isDefined).filter(_.runtimeMinutes.isDefined)
-    val flatMapped: List[TitleBasics] = noneGenresAndRuntimesRemoved.flatMap(titleBasics => constructTitleBasicsListForAllGenresFromSingleObject(titleBasics))
+    val noneGenresAndRuntimesRemoved = list.filter(entry => entry.genres.isDefined && entry.runtimeMinutes.isDefined)
+    val flatMapped: List[TitleBasics] = noneGenresAndRuntimesRemoved.flatMap(titleBasics => titleBasics.genres.get.map(genre => titleBasics.copy(genres = Some(List(genre)))))
     val groupedByGenre: Map[String, List[TitleBasics]] = flatMapped.groupBy[String](_.genres.get.head)
     groupedByGenre.map{case (genre, titleBasicsList) => getAverageMinMaxRuntimesForTitleBasicsListAndGenre(titleBasicsList, genre)}.toList
   }
@@ -77,12 +68,78 @@ object ImdbAnalysis {
     For the titles use the primaryTitle field and account only for entries whose titleType is ‘movie’.
   */
 
+
   def task2(l1: List[TitleBasics], l2: List[TitleRatings]): List[String] = {
-    null
+
+    val titleRatingsTconstMap: Map[String, TitleRatings] = l2.foldLeft(Map[String, TitleRatings]())((acc, titleRatings) => acc + (titleRatings.tconst -> titleRatings))
+
+    l1.filter( titleBasics => {
+      titleBasics.primaryTitle.isDefined && titleBasics.startYear.isDefined && titleBasics.titleType.isDefined && titleBasics.startYear.get >= 1990 && titleBasics.startYear.get <= 2018 && titleBasics.titleType.get == "movie"  && titleRatingsTconstMap.contains(titleBasics.tconst) && titleRatingsTconstMap(titleBasics.tconst).averageRating >= 7.5 && titleRatingsTconstMap(titleBasics.tconst).numVotes >= 500000
+    }).map(_.primaryTitle.get)
+  }
+
+  /*
+  Return the top rated movie of each genre for each decade between 1900 and 1999.
+  For the titles use the primaryTitle field and account only for entries whose titleType is ‘movie’.
+  For calculating the top rated movies use the averageRating field and for the release year use the startYear field.
+  The output should be sorted by decade and then by genre.
+  For the movies with the same rating and of the same decade, print only the one with the title that comes first alphabetically.
+  Each decade should be represented with a single digit, starting with 0 corresponding to 1900-1909.
+      return type:
+        List[(Int, String, String)]
+          decade:Int
+          genre:String
+          title:String
+   */
+  /*
+    Class defs (for readability):
+          case class TitleBasics(tconst: String, titleType: Option[String], primaryTitle: Option[String],
+                      originalTitle: Option[String], isAdult: Int, startYear: Option[Int], endYear: Option[Int],
+                      runtimeMinutes: Option[Int], genres: Option[List[String]])
+          case class TitleRatings(tconst: String, averageRating: Float, numVotes: Int)
+   */
+
+  def getDecade(titleBasics: TitleBasics): Int = (titleBasics.startYear.get / 10) % 10
+
+  def isEligibleTask3(tb: TitleBasics, titleRatingsTconstMap :  Map[String, TitleRatings]): Boolean = {
+    titleRatingsTconstMap.contains(tb.tconst) && tb.startYear.isDefined && tb.primaryTitle.isDefined && tb.titleType.isDefined && tb.genres.isDefined && tb.startYear.get >= 1900 && tb.startYear.get <= 1999 && tb.titleType.get == "movie"
+  }
+
+  def getTopRatedTitle(titleBasicsList: List[TitleBasics], titleRatingsTconstMap :  Map[String, TitleRatings]): String = {
+    titleBasicsList.foldRight[(String, Float)](("",0.0f))((titleBasics, accPair) => {
+
+      if (titleRatingsTconstMap(titleBasics.tconst).averageRating > accPair._2) {
+
+        (titleBasics.primaryTitle.get, titleRatingsTconstMap(titleBasics.tconst).averageRating)
+
+      } else if (titleRatingsTconstMap(titleBasics.tconst).averageRating == accPair._2 && titleBasics.primaryTitle.get < accPair._1) {
+
+        (titleBasics.primaryTitle.get, titleRatingsTconstMap(titleBasics.tconst).averageRating)
+
+      } else accPair
+    })._1
   }
 
   def task3(l1: List[TitleBasics], l2: List[TitleRatings]): List[(Int, String, String)] = {
-    null
+    //  need a similar filter situation first. keep only items we know are in both titlebasics and titleratings.
+    //  relevant fields: tb.startYear, tb.primaryTitle, tb.titleType, tb.genres, tr.averageRating
+    //  filter by fields defined, start year between 1900 and 1999, and movie titletype
+    //  will need to group by genre AND group by decade?
+    val titleRatingsTconstMap : Map[String, TitleRatings] = l2.foldLeft(Map[String, TitleRatings]())((acc, titleRatings) => acc + (titleRatings.tconst -> titleRatings))
+
+    val titleBasicsFiltered : List[TitleBasics] = l1.filter(isEligibleTask3(_, titleRatingsTconstMap))
+
+    val genreFlatMapped : List[TitleBasics] = titleBasicsFiltered.flatMap(titleBasics => titleBasics.genres.get.map(genre => titleBasics.copy(genres = Some(List(genre)))))
+
+    val groupedByDecade : Map[Int, List[TitleBasics]] = genreFlatMapped.groupBy[Int](getDecade)
+
+    val decadeGenreMap : Map[Int, Map[String, List[TitleBasics]]] = groupedByDecade.mapValues(_.groupBy(_.genres.get.head))
+
+    //  TODO need to sort my maps by decade then genre, converting to the nested pair structure to maintain order.
+    //  TODO fold through sorted structure, finding the top rated movie of each decade-genre (implicitly maintaining alphabetical order with comparison)
+
+    val sortedNestedStructure : List[(Int, List[(String, List[TitleBasics])])] = decadeGenreMap.mapValues(_.toList.sortBy(_._1)).toList.sortBy(_._1)
+    sortedNestedStructure.flatMap(decadeListPair => (List.fill(decadeListPair._2.length)(decadeListPair._1), decadeListPair._2.map(genreTbListPair => (genreTbListPair._1, getTopRatedTitle(genreTbListPair._2, titleRatingsTconstMap)))).zipped.toList).map{case (decade, (genre, title)) => (decade, genre, title)}
   }
 
   // Hint: There could be an input list that you do not really need in your implementation.
@@ -91,13 +148,13 @@ object ImdbAnalysis {
   }
 
   def main(args: Array[String]) {
-    val durations = timed("Task 1", task1(titleBasicsList))
-    // val titles = timed("Task 2", task2(titleBasicsList, titleRatingsList))
-    // val topRated = timed("Task 3", task3(titleBasicsList, titleRatingsList))
+//    val durations = timed("Task 1", task1(titleBasicsList))
+//    val titles = timed("Task 2", task2(titleBasicsList, titleRatingsList))
+     val topRated = timed("Task 3", task3(titleBasicsList, titleRatingsList))
     // val crews = timed("Task 4", task4(titleBasicsList, titleCrewList, nameBasicsList))
-    println(durations)
-    // println(titles)
-    // println(topRated)
+//    println(durations)
+//    println(titles)
+    println(topRated)
     // println(crews)
     println(timing)
   }
